c003220cd08ec9494ecc8ec811da22ef0ccd0ad9
Aditya Nigam <nigamadi2209@gmail.com>
2022-07-20 01:23:57 +0530

    some changes by Agrasth

--- util/database.js
+++ util/database.js
@@ -1,20 +1,33 @@
 const mongodb = require('mongodb');
-const MongoClient = mongodb.MongoClient;
+// const MongoClient = mongodb.MongoClient;
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
 
 //the underscore is only for telling that this file will only be used internally
 let _db;
 
 const mongoConnect = callback => {
-    MongoClient.connect('mongodb+srv://Adi2209:adiisthebest@cluster0.0xzww.mongodb.net/shop?retryWrites=true&w=majority')
-        .then(client => {
-            console.log('Connected!');
-            _db = client.db();
-            callback();
-        })
-        .catch(err => {
-            console.log(err)
-            throw err;
-        });
+    // MongoClient.connect('mongodb+srv://Adi2209:adiisthebest@cluster0.0xzww.mongodb.net/shop?retryWrites=true&w=majority')
+    //     .then(client => {
+    //         console.log('Connected!');  
+    //         _db = client.db();
+    //         callback();
+    //     })
+    //     .catch(err => {
+    //         console.log('!');  
+    //         console.log(err)
+    //         throw err;
+    //     });
+    const uri = "mongodb+srv://Adi2209:<password>@cluster0.0xzww.mongodb.net/?retryWrites=true&w=majority";
+    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+    client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    // perform actions on the collection object
+        console.log('Connected!');
+        _db = client.db();
+        // callback();
+        client.close();
+    });
 
 };
 
